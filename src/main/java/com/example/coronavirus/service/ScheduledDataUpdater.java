c527c2e95927d01930e5c0763bbad27c1be4d245
Oleg Pavlyukov <cpabox777@gmail.com>
2020-04-11 19:14:09 +0400

    Fix schedule db updater

--- src/main/java/com/example/coronavirus/service/ScheduledDataUpdater.java
+++ src/main/java/com/example/coronavirus/service/ScheduledDataUpdater.java
@@ -3,7 +3,9 @@ package com.example.coronavirus.service;
 import com.example.coronavirus.foreignDataSource.ForeignDataSource;
 import com.example.coronavirus.foreignDataSource.exception.ResourceNotAvailableException;
 import com.example.coronavirus.exception.DataInitException;
+import com.example.coronavirus.model.Country;
 import com.example.coronavirus.model.DailyStatistic;
+import com.example.coronavirus.repository.CountryRepository;
 import com.example.coronavirus.repository.DailyStatRepository;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -32,19 +34,23 @@ import java.util.stream.Collectors;
 @EnableScheduling
 public class ScheduledDataUpdater {
 
-    private DailyStatRepository repository;
+    private DailyStatRepository dsRepository;
     private ForeignDataSource foreignDataSource;
+    private CountryRepository countryRepository;
 
     @Autowired
-    public ScheduledDataUpdater(DailyStatRepository repository, ForeignDataSource foreignDataSource) {
-        this.repository = repository;
+    public ScheduledDataUpdater(DailyStatRepository dsRepository,
+                                ForeignDataSource foreignDataSource,
+                                CountryRepository countryRepository) {
+        this.dsRepository = dsRepository;
         this.foreignDataSource = foreignDataSource;
+        this.countryRepository = countryRepository;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void initDataBase() {
         log.debug("Start initDataBase()");
-        long recordsCount= repository.count();
+        long recordsCount= dsRepository.count();
         if (recordsCount < 1) {
             try {
                 List<DailyStatistic> freshDataList = foreignDataSource.getStatsByAllCountries();
@@ -52,11 +58,10 @@ public class ScheduledDataUpdater {
                         key-> key.getCountry().getName()));
                 int count = 1;
                 for (List<DailyStatistic> dailyStatisticList : mapToSave.values()) {
-                    repository.saveAll(dailyStatisticList);
+                    dsRepository.saveAll(dailyStatisticList);
                     Thread.sleep(10000);
                     System.out.println("Country num: " + ++count + ". Time: " + LocalDateTime.now());
                 }
-//                repository.saveAll(freshDataList);
             } catch (ResourceNotAvailableException | InterruptedException e) {
                 log.error("Fatal error during init db. No data available.", e);
                 throw new DataInitException("Fatal error during init db. No data available.", e);
@@ -69,20 +74,26 @@ public class ScheduledDataUpdater {
         log.info("Refreshing data at " + LocalDateTime.now());
         List<DailyStatistic> updateList = new ArrayList<>();
         try {
-            if (repository.count() < 1) return;
+            if (dsRepository.count() < 1) return;
 
-            List<DailyStatistic> repositoryDsList = repository.findAllByDate(LocalDate.now());
+            List<DailyStatistic> repositoryDsList = dsRepository.findAllByDate(LocalDate.now().minusDays(1));
             List<DailyStatistic> foreignDsList = foreignDataSource.getCurrentDayWorldStat();
             if (repositoryDsList == null || repositoryDsList.isEmpty()) {
-                System.out.println("DB list empty");
-                repository.saveAll(foreignDsList);
+                for (DailyStatistic ds : foreignDsList) {
+                    Country country = countryRepository.findCountryByName(ds.getCountry().getName());
+                    if (country != null) {
+                        ds.setCountry(country);
+                        dsRepository.save(ds);
+                    } else {
+                        log.error("Country not found -> " + ds.getCountry().getName());
+                    }
+                }
                 return;
             }
             for (int i = 0; i < foreignDsList.size() - 1; i++) {
                 for (int j = 0; j < repositoryDsList.size() - 1; j++) {
                     DailyStatistic fds = foreignDsList.get(i);
                     DailyStatistic rds = repositoryDsList.get(j);
-                    System.out.println(rds + " " + fds.toString());
                     if (fds.getCountry().getName().equals(rds.getCountry().getName())) {
                         if (fds.getDate().isEqual(rds.getDate())) {
                             if (fds.getCases() != rds.getCases()
@@ -99,7 +110,7 @@ public class ScheduledDataUpdater {
             }
             if (!updateList.isEmpty()) {
                 System.out.println("Update data");
-                repository.saveAll(updateList);
+                dsRepository.saveAll(updateList);
             }
         } catch (ResourceNotAvailableException e) {
             log.error("Refresh data error", e);
